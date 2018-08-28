cdc7a1fccbd9bb10794e02504a18a60eeef887b4
zhouzhu85 <734837014@qq.com>
2018-08-28 18:14:35 +0800

    #springboot# 简单的redis消息队列-发布者

---
+++ springboot-redis-publisher/src/main/java/com/github/zhouzhu/SpringbootRedisMessage2Application.java
@@ -0,0 +1,12 @@
+package com.github.zhouzhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisMessage2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedisMessage2Application.class, args);
+	}
+}
