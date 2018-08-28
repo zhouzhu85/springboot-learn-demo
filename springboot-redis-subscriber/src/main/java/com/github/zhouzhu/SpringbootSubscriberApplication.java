c362e26b23266ca41c7e1c68ea9366455859828b
zhouzhu85 <734837014@qq.com>
2018-08-28 18:15:27 +0800

    #springboot# 简单的redis消息队列-订阅者

---
+++ springboot-redis-subscriber/src/main/java/com/github/zhouzhu/SpringbootSubscriberApplication.java
@@ -0,0 +1,12 @@
+package com.github.zhouzhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSubscriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSubscriberApplication.class, args);
+	}
+}
